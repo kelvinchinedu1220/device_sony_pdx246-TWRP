d046f447db2befdcb6499673b7205cceb12eb20e
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-01-05 08:56:09 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,10 @@ DEVICE_PATH := device/sony/pdx246
 # Building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+# Broken rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
