63355113b17328183b1c24006a2e579b6fdda1f7
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-01-05 07:50:36 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,94 +1,39 @@
-# Copyright (C) 2017-2023 The Android Open Source Project
-# Copyright (C) 2014-2023 The Team Win LLC
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
 # SPDX-License-Identifier: Apache-2.0
+#
 
-# Inherit from those products. Most specific first.
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Configure core_64_bit.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
+# Configure virtual_ab_ota launch_with_vendor_ramdisk.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Enable updating of APEXes
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+# Configure twrp common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+PRODUCT_PACKAGES += \
+    bootctrl.sony_sm6450.recovery \
+    android.hardware.boot@1.2-impl-qti.recovery
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# SHIPPING API
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# VNDK API
+PRODUCT_TARGET_VNDK_VERSION := 35
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# SHIPPING API
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-# VNDK API
-PRODUCT_TARGET_VNDK_VERSION := 31
-
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script
-
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    bootctrl.sony_pdx246.recovery \
-    android.hardware.boot@1.1-impl-qti.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
-#  Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
-
-# Fastbootd
-TW_INCLUDE_FASTBOOTD := true
-
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock
-
-# Screen
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/qcom-caf/bootctrl
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libcuuc.so
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    libxml2 \
-    libandroidicu
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.device=$(PRODUCT_RELEASE_NAME)
+TWRP_REQUIRED_MODULES += \
+    sony_prebuilt
