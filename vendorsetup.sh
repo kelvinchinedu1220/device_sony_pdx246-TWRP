922f94b692c2edbeb864312f5d5022507ed54a10
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-01-10 04:06:29 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2019-2021 The OrangeFox Recovery Project
-#
+# 	Copyright (C) 2019-2020 The OrangeFox Recovery Project
+#	
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
@@ -14,7 +14,7 @@
 #
 # 	This software is released under GPL version 3 or any later version.
 #	See <http://www.gnu.org/licenses/>.
-#
+# 	
 # 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="pdx246"
@@ -34,57 +34,27 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-    
+
+	# set maintainer
+	export OF_MAINTAINER="nino"
+
+        # use system (ROM) fingerprint where available
+        export OF_USE_SYSTEM_FINGERPRINT=1
+
+	# OTA for custom ROMs
+        #export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+        #export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+        
+        # Fix display icons
+        export OF_SCREEN_H=2560
+
+	# SKIP FBE Decryption (TEMP FIX)
+	export OF_SKIP_FBE_DECRYPTION=1
+
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	
-	# pdx246 Only!
-	export TARGET_DEVICE_ALT="pdx246"
-	export OF_TARGET_DEVICES="pdx246"
-	
-	export FOX_R11=1
-	export OF_PATCH_AVB20=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export FOX_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export OF_FBE_METADATA_MOUNT_IGNORE=1
-	export FOX_DELETE_AROMAFM=1
-    export OF_USE_SYSTEM_FINGERPRINT=1
-    export OF_CHECK_OVERWRITE_ATTEMPTS=1
-
-    # Screen Settings
-    export OF_ALLOW_DISABLE_NAVBAR=0
-
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_USE_GREEN_LED=1
-
-    export OF_USE_TWRP_SAR_DETECT="1"
-
-	# OTA for Custom ROMs
-    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
-    export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
-
-	# Quick Backup Defaults
-    export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
-
-	# Run a process after formatting data to work-around MTP issues
-	export OF_RUN_POST_FORMAT_PROCESS=1
-
 	export LC_ALL="C"
+
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
@@ -95,4 +65,3 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
  	fi
 
 fi
-#
